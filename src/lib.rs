4354764c0f1b34480f29c58f9dae8f95ab4754e7
SilverWingedSeraph <lfstindall@gmail.com>
2017-04-03 20:34:40 -0500

    Finalize API for assembly

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,25 @@
+//! mlem-asm is an assembler library for MLeM, the Machine Learning Machine.
+//! This is a Harvard architecture machine with immutable programs, 8 general purpouse registers,
+//! and a hardware-supported stack with stack and base pointers.
+//! 
+//! # Assembly Language
+//! mlem-asm assembles the mlasm language into CBOR-encoded data that can be read and executed by MLeM.
+//! The mlasm language looks like so:
+//! ```asm
+//! ; Anything following a semicolon is a comment.
+//! ; Lines can be terminated with a semicolon, or not. The following two lines are equivalent:
+//! noop
+//! noop;
+//! ; Instructions that require arguments look like so:
+//! move R:R0 R:R1 ; Set R1 equal to R0
+//! ```
+
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_cbor;
 
 extern crate mlem;
-use mlem::{Address, Instruction, Register};
+pub use mlem::{Address, Instruction, Register, Program};
 
 #[cfg(test)]
 mod test;
@@ -21,99 +37,111 @@ enum InstructionName {
     None,
 }
 
-fn parse_instruction(name: &str) -> Option<InstructionName> {
+fn parse_instruction(name: &str) -> Result<InstructionName, String> {
     use std::borrow::Borrow;
     match name.to_lowercase().borrow() {
-        "noop" => Some(InstructionName::NoOp),
-        "halt" => Some(InstructionName::Halt),
-        "illegal" | "illeg" | "illg" => Some(InstructionName::Illegal),
-        "zero" => Some(InstructionName::Zero),
-        "move" => Some(InstructionName::Move),
-        "output" | "oput" => Some(InstructionName::Output),
-        "input" | "iput" => Some(InstructionName::Input),
-        "add" | "uadd" => Some(InstructionName::Add),
-        "subtract" | "usub" | "sub" => Some(InstructionName::Sub),
-        _ => None
+        "noop" => Ok(InstructionName::NoOp),
+        "halt" => Ok(InstructionName::Halt),
+        "illegal" | "illeg" | "illg" => Ok(InstructionName::Illegal),
+        "zero" => Ok(InstructionName::Zero),
+        "move" => Ok(InstructionName::Move),
+        "output" | "oput" => Ok(InstructionName::Output),
+        "input" | "iput" => Ok(InstructionName::Input),
+        "add" | "uadd" => Ok(InstructionName::Add),
+        "subtract" | "usub" | "sub" => Ok(InstructionName::Sub),
+        other => Err(format!("Unknown instruction: {}", other))
     }
 }
 
-fn parse_register_name(item: &str) -> Option<Register> { 
+fn parse_register_name(item: &str) -> Result<Register, String> { 
     use std::borrow::Borrow;
     match item.to_lowercase().borrow() {
-        "r0" => Some(Register::R0),
-        "r1" => Some(Register::R1),
-        "r2" => Some(Register::R2),
-        "r3" => Some(Register::R3),
-        "r4" => Some(Register::R4),
-        "r5" => Some(Register::R5),
-        "r6" => Some(Register::R6),
-        "r7" => Some(Register::R7),
-        "sp" => Some(Register::SP),
-        "bp" => Some(Register::BP),
-        _ => None
+        "r0" => Ok(Register::R0),
+        "r1" => Ok(Register::R1),
+        "r2" => Ok(Register::R2),
+        "r3" => Ok(Register::R3),
+        "r4" => Ok(Register::R4),
+        "r5" => Ok(Register::R5),
+        "r6" => Ok(Register::R6),
+        "r7" => Ok(Register::R7),
+        "sp" => Ok(Register::SP),
+        "bp" => Ok(Register::BP),
+        other => Err(format!("Unknown register name: {}", other))
     }
  }
 
-fn parse_literal(item: &str) -> Option<u64> { 
+fn parse_literal(item: &str) -> Result<u64, String> { 
     match u64::from_str_radix(item, 10) {
-        Ok(v) => Some(v),
-        Err(_) => None
+        Ok(v) => Ok(v),
+        Err(e) => Err(format!("Could not parse literal: {}", e))
     }
  }
 
 // Parse an address of the form "specifier:item" where specifier is one of "R", "L", "M", or "P"
 // and item is an appropriate specifier.
-fn parse_address(name: &str) -> Option<Address> {
+fn parse_address(name: &str) -> Result<Address, String> {
     let pieces: Vec<_> = name.split(':').collect();
-    if pieces.len() != 2 { return None; }
+    if pieces.len() != 2 { return Err("Malformed address.".into()); }
     use std::borrow::Borrow;
     let specifier = pieces[0];
     let item = pieces[1];
     match specifier.to_lowercase().borrow() {
         "r" => {
             match parse_register_name(item) {
-                Some(r) => Some(Address::RegAbs(r)),
-                None => None
+                Ok(r) => Ok(Address::RegAbs(r)),
+                Err(e) => Err(e)
             }
         },
         "l" => { 
             match parse_literal(item) {
-                Some(l) => Some(Address::Literal(l)),
-                None => None
+                Ok(l) => Ok(Address::Literal(l)),
+                Err(e) => Err(e)
             }
         },
         "m" => { 
             match parse_literal(item) {
-                Some(l) => Some(Address::MemAbs(l)),
-                None => None
+                Ok(l) => Ok(Address::MemAbs(l)),
+                Err(e) => Err(e)
             } 
         },
         "p" => { 
             match parse_register_name(item) {
-                Some(r) => Some(Address::MemReg(r)),
-                None => None
+                Ok(r) => Ok(Address::MemReg(r)),
+                Err(e) => Err(e)
             }
         }
-        _ => None
+        other => Err(format!("Unknown address type specifier: {}", other))
 
     }
 }
 
-// Parse a line of the form:`
-// instruction [operand1] [operand2] [operand3][; [comment text]]
-pub fn parse_line(line: &str) -> Option<Instruction> {
+/// Parse a line of the form `instruction [operand1] [operand2] [operand3][;[comment text]]`
+///
+/// The return value is a `Result<Option<Instruction>, String>`. An `Ok(Some(_))` value means a valid
+/// instruction (for instance, the line `move R:R0 R:R1`). An `Err(_)` value means that there is
+/// unparsable about the line (like `move R:R0 R:r1 garbage garbage`); an `Ok(None)` value means that
+/// the line was legal but meant nothing (like `; comment only`).
+/// # Examples
+/// Simple single-line parsing:
+/// ```
+/// use mlem_asm::{parse_line, Instruction};
+/// assert!(parse_line("noop") == parse_line("noop;"));
+/// assert!(parse_line("noop") == Ok(Some(Instruction::NoOp)));
+/// assert!(parse_line("") == Ok(None));
+/// ```
+pub fn parse_line(line: &str) -> Result<Option<Instruction>, String> {
     // Split off comments portion
     let semantic_portion;
     match line.split(';').next() {
         Some(v) => { semantic_portion = v; },
-        None => { return None; }
+        None => { return Ok(None); }
     };
     
     // Split into "words"
     let pieces: Vec<_> = semantic_portion.split_whitespace().collect();
 
     // If there are no words, this line is useless.
-    if pieces.len() == 0 { return None; }
+    if pieces.len() == 0 { return Ok(None); }
 
     let mut instruction_name = InstructionName::None;
     let mut arg1 = None;
@@ -122,38 +150,38 @@ pub fn parse_line(line: &str) -> Option<Instruction> {
     // Parse the name of the instruction.
     if pieces.len() >= 1 {
         match parse_instruction(pieces[0]) {
-            Some(v) => { instruction_name = v; },
-            None => { return None; }
+            Ok(v) => { instruction_name = v; },
+            Err(e) => { return Err(e); }
         };
     }
 
     // A single piece means a no-arg instruction; it can go straight to an Instruction.
     if pieces.len() == 1 {
         return match instruction_name {
-            InstructionName::NoOp => Some(Instruction::NoOp),
-            InstructionName::Halt => Some(Instruction::Halt),
-            InstructionName::Illegal => Some(Instruction::Illegal),
-            _ => None
+            InstructionName::NoOp => Ok(Some(Instruction::NoOp)),
+            InstructionName::Halt => Ok(Some(Instruction::Halt)),
+            InstructionName::Illegal => Ok(Some(Instruction::Illegal)),
+            _ => Err("Wrong number of arguments. Got 0.".into())
         };
     }
 
     // More than one word means the args need parsed.
     if pieces.len() >= 2 {
-        match parse_address(pieces[1]) {
-            Some(v) => { arg1 =Some(v); },
-            None => { return None; }
+        match parse_address(pieces[1].trim()) {
+            Ok(v) => { arg1 = Some(v); },
+            Err(e) => { return Err(e); }
         };
     }
     if pieces.len() >= 3 {
-        match parse_address(pieces[2]) {
-            Some(v) => { arg2 = Some(v); },
-            None => { return None; }
+        match parse_address(pieces[2].trim()) {
+            Ok(v) => { arg2 = Some(v); },
+            Err(e) => { return Err(e); }
         };
     }
     if pieces.len() >= 4 {
-        match parse_address(pieces[3]) {
-            Some(v) => { arg3 = Some(v); },
-            None => { return None; }
+        match parse_address(pieces[3].trim()) {
+            Ok(v) => { arg3 = Some(v); },
+            Err(e) => { return Err(e); }
         };
     }
 
@@ -162,10 +190,10 @@ pub fn parse_line(line: &str) -> Option<Instruction> {
         // Alias arg1 to its inner value, which DEFINITELY exists at this point.
         let arg1 = arg1.unwrap();
         return match instruction_name {
-            InstructionName::Zero => Some(Instruction::Zero(arg1)),
-            InstructionName::Input => Some(Instruction::Input(arg1)),
-            InstructionName::Output => Some(Instruction::Output(arg1)),
-            _ => None
+            InstructionName::Zero => Ok(Some(Instruction::Zero(arg1))),
+            InstructionName::Input => Ok(Some(Instruction::Input(arg1))),
+            InstructionName::Output => Ok(Some(Instruction::Output(arg1))),
+            _ => Err("Wrong number of arguments. Got 1.".into())
         };
     }
 
@@ -175,12 +203,81 @@ pub fn parse_line(line: &str) -> Option<Instruction> {
         let arg1 = arg1.unwrap();
         let arg2 = arg2.unwrap();
         return match instruction_name {
-            InstructionName::Move => Some(Instruction::Move(arg1, arg2)),
-            InstructionName::Add => Some(Instruction::Add(arg1, arg2)),
-            InstructionName::Sub => Some(Instruction::Sub(arg1, arg2)),
-            _ => None
+            InstructionName::Move => Ok(Some(Instruction::Move(arg1, arg2))),
+            InstructionName::Add => Ok(Some(Instruction::Add(arg1, arg2))),
+            InstructionName::Sub => Ok(Some(Instruction::Sub(arg1, arg2))),
+            _ => Err("Wrong number of arguments. Got 2.".into())
         }
     }
-    
-    None
+
+    Err("Malformed. Perhaps there are too many terms?".into())
+}
+
+fn initial_parse_program(program: &str) -> Vec<Result<Option<Instruction>, String>> {
+    let lines = program.lines();
+    let mut v = Vec::new();
+    for line in lines {
+        match parse_line(line) {
+            Ok(i) => { v.push(Ok(i)); }
+            Err(e) => { v.push(Err(e)); }
+        }
+    }
+    v
+}
+
+/// Parse an entire program, returning either a ready-to-execute MLeM program or
+/// a Vec of error messages, with line numbers, of all errors in the program.
+/// # Example
+/// A valid program:
+/// ```
+/// use mlem_asm::*;
+/// let valid_program = "
+///    noop
+///    move R:R0 R:SP;
+///    input R:R0;
+///    ; comment only
+///
+///    ";
+///    let expected_program = Ok(vec![
+///            Instruction::NoOp,
+///            Instruction::Move(Address::RegAbs(Register::R0), Address::RegAbs(Register::SP)),
+///            Instruction::Input(Address::RegAbs(Register::R0))
+///    ]);
+///    let program = parse_program(valid_program);
+///    assert!(program == expected_program, "Program resulted in: {:?} not: {:?}", program, expected_program);
+/// ```
+///
+/// An invalid program:
+/// ```
+/// use mlem_asm::*;
+/// let invalid_program = "
+///    noop
+///    move R:R0 R:xx;
+///    output invalid;
+///    ; comment only
+///
+///    ";
+///    let expected_errors = Err(vec![(2, "Unknown register name: xx".into()), (3, "Malformed address.".into())]);
+///    let errors = parse_program(invalid_program);
+///    assert!(errors == expected_errors, "Program resulted in: {:?} not: {:?}", errors, expected_errors);
+/// ```
+pub fn parse_program(program: &str) -> Result<Program, Vec<(u64, String)>> {
+    let mut p = Vec::new();
+    let mut errors = Vec::new();
+    for (n, line) in initial_parse_program(program).into_iter().enumerate() {
+        match line {
+            Ok(v) => {
+                if let Some(i) = v { p.push(i) };
+            },
+            Err(e) => {
+                errors.push((n as u64, format!("{}", e)));
+            }
+        };
+    }
+    if errors.len() == 0 {
+        // No errors!
+        Ok(p)
+    } else {
+        Err(errors)
+    }
 }
--- src/test.rs
+++ src/test.rs
@@ -1,22 +1,56 @@
-use parse_line;
+use {parse_line, parse_program};
 use mlem::{Instruction, Register, Address};
 #[test]
 fn test() {
-    assert!(parse_line("") == None);
-    assert!(parse_line("noop") == Some(Instruction::NoOp));
-    assert!(parse_line("noop; some comments") == Some(Instruction::NoOp));
+    assert!(parse_line("").unwrap() == None);
+    assert!(parse_line("noop").unwrap() == Some(Instruction::NoOp));
+    assert!(parse_line("noop; some comments").unwrap() == Some(Instruction::NoOp));
 }
 
 #[test]
 fn test_single_argument() {
-    assert!(parse_line("iput R:R0") == Some(Instruction::Input(Address::RegAbs(Register::R0))));
-    assert!(parse_line("oput R:R0 R:R1") == None);
+    assert!(parse_line("iput R:R0").unwrap() == Some(Instruction::Input(Address::RegAbs(Register::R0))));
+    parse_line("oput R:R0 R:R1").unwrap_err();
 }
 
 #[test]
 fn test_double_argument() {
-    assert!(parse_line("move R:R0 R:R1") == Some(Instruction::Move(
+    assert!(parse_line("move R:R0 R:R1").unwrap() == Some(Instruction::Move(
         Address::RegAbs(Register::R0),
         Address::RegAbs(Register::R1),
         )), "It was {:?}", parse_line("move R:R0 R:R1"));
+}
+
+#[test]
+fn test_parse_invalid_program() {
+    let invalid_program = "
+    noop;
+    move R:R0 R:R1;
+    move R:Rx R:R1;
+    move R:R0 L:1024p;
+    ";
+    let expected_errors = Err(vec![
+            (3, "Unknown register name: rx".into()),
+            (4, "Could not parse literal: invalid digit found in string".into())
+    ]);
+    let errors = parse_program(invalid_program);
+    assert!(errors == expected_errors, "Program resulted in: {:?} not: {:?}", errors, expected_errors);
+}
+
+#[test]
+fn test_parse_valid_program() {
+    let valid_program = "
+    noop
+    move R:R0 R:SP;
+    input R:R0;
+    ; comment only
+
+    ";
+    let expected_program = Ok(vec![
+            Instruction::NoOp,
+            Instruction::Move(Address::RegAbs(Register::R0), Address::RegAbs(Register::SP)),
+            Instruction::Input(Address::RegAbs(Register::R0))
+    ]);
+    let program = parse_program(valid_program);
+    assert!(program == expected_program, "Program resulted in: {:?} not: {:?}", program, expected_program);
 }
