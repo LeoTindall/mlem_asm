2bcc6baebd40bdbb767dd8ae657cbf49620d0724
SilverWingedSeraph <lfstindall@gmail.com>
2017-04-27 12:10:20 -0500

    Fix imports

--- test.asm
+++ test.asm
@@ -1,4 +1,6 @@
-noop;
-noop;
-
-; just a comment
+; a simple program that loops 1000 times
+move L:1001 R:R7    ; 0
+add R:R0 L:1        ; 1
+output R:R0         ; 2
+sub R:R7 L:1        ; 3
+jnz L:1 R:R7        ; 4 Loop if the counter is not 0
